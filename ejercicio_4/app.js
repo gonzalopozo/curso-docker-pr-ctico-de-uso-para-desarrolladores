2a53c008807393228f9b1a7264c080335ed796d4
gonzalopozo <gonzalopozo2005@gmail.com>
2024-09-15 11:51:02 +0200

    Añadir pruebas automáticas al código

--- ejercicio_4/app.js
+++ ejercicio_4/app.js
@@ -1,10 +1,7 @@
 const cron = require('node-cron');
-
-let times = 0;
-
-cron.schedule('1-59/5 * * * * *', () => {
-    times++;
-    console.log('Tick cada multiplo de 5: ', times);
-});
+const { syncDB } = require('./tasks/sync-db');
 
 console.log('Inicio de la app');
+
+
+cron.schedule('1-59/5 * * * * *', syncDB );
